ed4877f1641d21901b036b109b0b51d7e9c632ab
Jason <jason.malan@digital2dna.com>
2026-05-28 08:52:31 +0200

    defines first glance

--- temperatures-attempt.c
+++ temperatures-attempt.c
@@ -22,6 +22,10 @@
 
 #include <stdio.h>
 
+#define UPPER 300
+#define LOWER 0
+#define STEP 20
+
 int main()
 {
     // float fahr, celsius;
@@ -41,7 +45,7 @@ int main()
 
     int fahr;
 
-    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
     {
         printf("%3d %6.1f \n", fahr, ((5.0 / 9.0) * (fahr - 32.0)));
     }
