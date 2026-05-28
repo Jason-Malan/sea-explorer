1e7239904609df55ac38ed2618c6f9a256e42800
Jason <jason.malan@digital2dna.com>
2026-05-28 08:48:23 +0200

    temps in reverse

--- temperatures-attempt.c
+++ temperatures-attempt.c
@@ -24,19 +24,26 @@
 
 int main()
 {
-    float fahr, celsius;
-    float lower, upper, step;
+    // float fahr, celsius;
+    // float lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
+    // lower = 0;
+    // upper = 300;
+    // step = 20;
 
-    fahr = lower;
-    while (fahr <= upper)
+    // fahr = lower;
+    // while (fahr <= upper)
+    // {
+    //     celsius = (5.0 / 9.0) * (fahr - 32.0);
+    //     printf("%3.0f %6.1f\n", fahr, celsius);
+    //     fahr = fahr + step;
+    // }
+
+    int fahr;
+
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
     {
-        celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+        printf("%3d %6.1f \n", fahr, ((5.0 / 9.0) * (fahr - 32.0)));
     }
 
     return 1;
