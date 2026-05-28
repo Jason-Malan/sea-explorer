ec570ae953ee11614ade1966e66532b071e50294
Jason <jason.malan@digital2dna.com>
2026-05-28 09:39:29 +0200

    counting tabs, newlines and blanks

--- char-counter.c
+++ char-counter.c
@@ -1,9 +1,50 @@
 #include <stdio.h>
 
+int blanks_tabs_newlines_counter()
+{
+    int blanks = 0, tabs = 0, newlines = 0;
+    int c;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ')
+        {
+            ++blanks;
+        }
+        if (c == '\t')
+        {
+            ++tabs;
+        }
+        if (c == '\n')
+        {
+            ++newlines;
+        }
+    }
+
+    printf("blanks: %d, tabs: %d, newlines %d", blanks, tabs, newlines);
+
+    return 1;
+}
+
 int main()
 {
-    double nc;
-    for (nc = 0; getchar() != EOF; ++nc)
-        ;
-    printf("%.0f\n", nc);
+    return blanks_tabs_newlines_counter();
+}
+
+int counter()
+{
+    int nc, nl;
+
+    nl = 0;
+    while ((nc = getchar()) != EOF)
+    {
+        if (nc == '\n')
+        {
+            ++nl;
+        }
+    }
+
+    printf("%d", nl);
+
+    return 1;
 }
