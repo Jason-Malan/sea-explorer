2e1d113393541bf6234f120faecba3da44247302
Jason <jason.malan@digital2dna.com>
2026-05-29 07:47:40 +0200

    char frequenct done

--- char_frequency_histogram.c
+++ char_frequency_histogram.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+#define MAX_CHARACTERS 128
+
+int main()
+{
+    int c;
+    int char_counter[MAX_CHARACTERS] = {0};
+
+    while ((c = getchar()) != EOF && c < MAX_CHARACTERS)
+    {
+        char_counter[c]++;
+    }
+
+    for (int i = 0; i < MAX_CHARACTERS; i++)
+    {
+        printf("%3d: %d \n", i, char_counter[i]);
+    }
+
+    return 0;
+}
