eb6e4c35b5aeec82e078092cb6852755dee5e109
Jason <jason.malan@digital2dna.com>
2026-06-03 10:46:01 +0200

    understanding char arrays intro

--- character_arrays.c
+++ character_arrays.c
@@ -1,39 +1,53 @@
 #include <stdio.h>
 #define MAXLINE 1000 /* maximum input line length */
-int getline(char line[], int maxline);
+
+int getline2(char line[], int maxline);
 void copy(char to[], char from[]);
+
 /* print the longest input line */
-main()
+int main()
 {
     int len;               /* current line length */
-    int max;               /* maximum length seen so far */
+    int max = 0;           /* maximum length seen so far */
     char line[MAXLINE];    /* current input line */
     char longest[MAXLINE]; /* longest line saved here */
-    max = 0;
-    while ((len = getline(line, MAXLINE)) > 0)
+
+    while ((len = getline2(line, MAXLINE)) > 0)
+    {
         if (len > max)
         {
             max = len;
             copy(longest, line);
         }
-    if (max > 0) /* there was a line */
-        printf("%s", longest);
+    }
+
+    if (max > 0)               /* there was a line */
+        printf("%s", longest); // todo: what does %s do?
+
     return 0;
 }
+
 /* getline: read a line into s, return length */
-int getline(char s[], int lim)
+int getline2(char s[], int lim)
 {
     int c, i;
+
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    {
         s[i] = c;
+    }
+
     if (c == '\n')
     {
         s[i] = c;
         ++i;
-    }
-    s[i] = '\0';
+    } // add back the new line
+
+    s[i] = '\0'; // null terminating operator for char arrays.
+
     return i;
 }
+
 /* copy: copy 'from' into 'to'; assume to is big enough */
 void copy(char to[], char from[])
 {
