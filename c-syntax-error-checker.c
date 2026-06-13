08eeccba300a2420c1e9042783c35299888bceff
Jason Malan <jasondm1291@gmail.com>
2026-06-13 19:25:31 +0200

    mathcing wrappers

---
+++ c-syntax-error-checker.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+/*
+    1.1 unmatched parentheses    ->  ()
+    1.2 unmatched brackets       ->  []
+    1.3 unmatched braces         ->  {}
+
+    2.1 quotes single
+    2.2 quotes double
+    2.3 escape sequences
+    
+    3.1 comments
+*/
+
+/*
+    
+*/
+
+#define MAX_IN_CHAR_STACK_DEPTH 10
+#define WRONG_SYN_MSG "WRONG SYNTAX \n"
+
+int main() {
+    char inCharStack[MAX_IN_CHAR_STACK_DEPTH] = {0};
+    int inCharStackIndex = 0;
+
+    int c;
+    while ((c = getchar()) != EOF) 
+    {
+        if (c == '(' || c == '[' || c == '{') 
+        {
+            inCharStack[inCharStackIndex++] = c;
+        }
+
+        if (c == ')') 
+        {
+            if ((inCharStackIndex == 0 || inCharStack[inCharStackIndex - 1] != '(')) 
+            {
+                printf("%s", WRONG_SYN_MSG);
+            } 
+            --inCharStackIndex;
+        }
+
+        if (c == ']') 
+        {
+            if ((inCharStackIndex == 0 || inCharStack[inCharStackIndex - 1] != '[')) 
+            {
+                printf("%s", WRONG_SYN_MSG);
+            } 
+            --inCharStackIndex;
+        } 
+
+        if (c == '}') 
+        {
+            if ((inCharStackIndex == 0 || inCharStack[inCharStackIndex - 1] != '{')) 
+            {
+                printf("%s", WRONG_SYN_MSG);
+            } 
+            --inCharStackIndex;
+        }
+    }
+
+    return 1;
+}
