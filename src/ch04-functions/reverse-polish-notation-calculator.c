4a89ef3da33936eaf558703542d6dd34c937b5ef
Jason David Malan <jason.malan@digital2dna.com>
2026-09-23 09:22:15 +0200

    added modulo support to rpn

--- src/ch04-functions/reverse-polish-notation-calculator.c
+++ src/ch04-functions/reverse-polish-notation-calculator.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>  /* for atof() */
 #include <ctype.h>
+#include <math.h>
 
 #define MAXOP   100  /* max size of operand or operator */
 #define NUMBER  '0'  /* signal that a number was found */
@@ -41,6 +42,10 @@ int main()
                 push(pop() / op2);
             else
                 printf("error: zero divisor\n");
+            break;
+        case '%':
+            op2 = pop();
+            push(fmod(pop(), op2));
             break;
         case '\n':
             printf("\t%.8g\n", pop());
